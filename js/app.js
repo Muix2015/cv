699fb500c14196e4167e0474e4e5e79211daf3c2
Xu Mi <mixer2010@126.com>
2017-09-13 09:40:15 +0800

    upload the first version of CV

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        sectionsColor: ['dodgerblue', 'lightgreen', 'teal', 'seagreen', 'sienna'],
+        navigation: true,
+        navigationPosition: 'left',
+    });
+});
